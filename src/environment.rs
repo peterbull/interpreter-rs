44ef1c222d43b8736e129508b47e7c8b11e69a63
peter bull <17122363+peterbull@users.noreply.github.com>
2026-01-30 05:58:56 -0500

    add enclosing logic to env

--- src/environment.rs
+++ src/environment.rs
@@ -15,12 +15,8 @@ impl Environment {
         }
     }
     pub fn update_values(&mut self, name: String, value: Value) -> Result<Value, ReefError> {
-        match self.values.insert(name, value) {
-            Some(value) => Ok(value),
-            None => Err(ReefError::reef_general_error(
-                "error creating or updating variable value",
-            )),
-        }
+        self.values.insert(name, value.clone());
+        Ok(value)
     }
 
     pub fn define(&mut self, name: String, value: Value) -> Result<Value, ReefError> {
@@ -29,20 +25,28 @@ impl Environment {
 
     pub fn assign(&mut self, name: String, value: Value) -> Result<Value, ReefError> {
         if self.values.contains_key(&name) {
-            self.update_values(name, value)
-        } else {
-            Err(ReefError::reef_general_error(&format!(
-                "undefined variable: {:?}",
-                name
-            )))
+            return self.update_values(name, value);
         }
+        if let Some(ref mut enc) = self.enclosing {
+            return enc.assign(name, value);
+        }
+        Err(ReefError::reef_general_error(&format!(
+            "undefined variable: {:?}",
+            name
+        )))
     }
 
-    pub fn get(&self, name: &Token) -> Value {
-        self.values
-            .get(&name.lexeme)
-            .expect("env should always be able to get this value")
-            .clone()
+    pub fn get(&self, name: &Token) -> Result<Value, ReefError> {
+        if let Some(val) = self.values.get(&name.lexeme) {
+            return Ok(val.clone());
+        }
+        if let Some(enc) = &self.enclosing {
+            return enc.get(name);
+        }
+        Err(ReefError::reef_general_error(&format!(
+            "undefined variable: '{}'",
+            name.lexeme
+        )))
     }
 }
 impl Default for Environment {
